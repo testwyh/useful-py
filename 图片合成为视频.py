1008ba7e9a935d7b8f3ef6f2880a5b0685151599
yaofanghao <85755132+yaofanghao@users.noreply.github.com>
2021-12-15 16:03:15 +0800

    修改/图片名按顺序1,2,3而不是1,10,11输出

--- 图片合成为视频.py
+++ 图片合成为视频.py
@@ -1,26 +1,21 @@
 import os
 import cv2
 
-path = 'ALL_JPEG/'  # 原图片的路径
+path = 'img_out/'  # 原图片的路径
 filelist = os.listdir(path)
 
-fps = 3  # 视频每秒帧数
-size = (640, 480)  # 需要转为视频的图片的尺寸，图片的尺寸多大，就写多大,例如:t1.jpg大小为480*640 （480,640）
+fps = 28.92 # 视频每秒帧数
+size = (544, 960)  # 需要转为视频的图片的尺寸，图片的尺寸多大
 # 可以使用cv2.resize()进行修改
 
 # 视频的名称为VideoTest1，格式为.avi
-video = cv2.VideoWriter("VideoOUT.avi",
+video = cv2.VideoWriter("kongpao.avi",
                         cv2.VideoWriter_fourcc(*'XVID'),
                         fps, size)
-            # MP4V MPEG-4编码 .mp4  要限制结果视频的大小，这是一个很好的选择。
-            # X264 MPEG-4编码  .mp4  想限制结果视频的大小，这可能是最好的选择。
-            # I420 该参数是YUV编码类型，文件名后缀为.avi   广泛兼容，但会产生大文件
-            # PIMI 该参数是MPEG-1编码类型，文件名后缀为.avi
-            # XVID 该参数是MPEG-4编码类型，文件名后缀为.avi  要限制结果视频的大小，这是一个很好的选择。
-            # THEO 该参数是Ogg Vorbis,文件名后缀为.ogv
-            # FLV1 该参数是Flash视频，文件名后缀为.flv
-
 # 视频保存在当前目录下
+
+#按顺序读取图片，这样保证是1,2,3...而不是1,10,11...
+filelist.sort(key=lambda x: int(x.split('.')[0]))
 
 for item in filelist:
     if item.endswith('.jpg'):
